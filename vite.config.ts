2505e3fe82ce3365040132bb6d7371683642393d
lipintolya <ttolyalipin@gmail.com>
2026-02-24 17:44:50 +0500

    fix: исправить обработку sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  // Отключаем обработку XML и TXT файлов как HTML
+  assetsInclude: ['**/*.xml', '**/*.txt'],
 })
